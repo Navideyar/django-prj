5eb1a00a559a5e3d494f40fca627dfd5bfed76cb
navideyar <nvd.sfy@gmail.com>
2025-02-11 23:15:58 +0330

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,12 @@
-from django.template import loader
+
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def contact(request):
-    template = loader.get_template('contact.html')
-    return HttpResponse(template.render())
+    return render(request, 'contact.html')
 
 def about(request):
-    template = loader.get_template('about.html')
-    return HttpResponse(template.render())
+    return render(request, 'about.html')
 
 def home(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    return render(request, 'home.html')
